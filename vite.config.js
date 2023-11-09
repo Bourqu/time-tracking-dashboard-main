666a2efc4f25e4592578bca5558856fe706b77bf
Bourqu <dybourqu@gmail.com>
2023-11-09 01:57:16 -0500

    re=ran build

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/time-tracking-dashboard-main/',
   test:{
     globals:true,
     environment:'jsdom',
     setupFiles: './tests/setup.js',
-    base:'https://bourqu.github.io/time-tracking-dashboard-main/'
   }
+  
 })
