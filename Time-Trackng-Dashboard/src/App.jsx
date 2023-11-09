88711898283561bf1fe2514da324518cd660ab3c
Bourqu <dybourqu@gmail.com>
2023-11-08 23:57:25 -0500

    deleted the original index

--- Time-Trackng-Dashboard/src/App.jsx
+++ Time-Trackng-Dashboard/src/App.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import './App.css'
+import Profile from './Components/Profile'
+import Cardgrid from "./Components/Cardgrid"
 
 const App = () => {
 
+  const [selectedStatus, setSelectedStatus] = useState("daily");
 
+  const handleStatusChange = (newStatus) => {
+    setSelectedStatus(newStatus);
+  };
   return (
-    <>
-
-    </>
+    <div className="App">
+      <Profile onStatusChange={handleStatusChange} selectedStatus={selectedStatus} />
+      <Cardgrid selectedStatus={selectedStatus} />
+    </div>
   );
 };
 
